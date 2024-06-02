5020fedc1f99953d4e1756ecc1da0f49dd53cc4f
Eduardo Kiriakos <eduardokiriakos@gmail.com>
2024-06-02 09:51:19 +0200

    feat: create AccountTable component

---
+++ src/accounts/components/AccountTable.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { CTable } from '@/components/core';
+import { TableHeader } from '@/components/core/CTable.vue';
+import { storeToRefs } from 'pinia';
+import { useAccountStore } from '@/accounts/stores';
+
+const accountStore = useAccountStore();
+const { accounts } = storeToRefs(accountStore);
+
+const headers: TableHeader[] = [
+  { text: 'bank', itemKey: 'bank.name' },
+  { text: 'account', itemKey: 'name' },
+  { text: 'amount', itemKey: 'amount' },
+]
+</script>
+
+<template>
+  <CTable
+    :headers="headers"
+    :items="accounts"
+  >
+    <template #item-amount="{ item }">
+      <span>{{ item.amount.toFixed(2) }} {{ item.currency.symbol }}</span>
+    </template>
+  </CTable>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,8 @@
 <script setup lang="ts">
-import { storeToRefs } from 'pinia';
-import { useAccountStore } from '@/accounts/stores';
-import { CTable } from '@/components/core';
-import { TableHeader } from '@/components/core/CTable.vue';
+import AccountTable from '@/accounts/components/AccountTable.vue';
 
-const accountStore = useAccountStore();
-const { accounts } = storeToRefs(accountStore);
-
-const headers: TableHeader[] = [
-  { text: 'bank', itemKey: 'bank.name' },
-  { text: 'account', itemKey: 'name' },
-  { text: 'amount', itemKey: 'amount' },
-]
 </script>
 
 <template>
-  <CTable
-    :headers="headers"
-    :items="accounts"
-  >
-    <template #item-amount="{ item }">
-      <span>{{ item.amount.toFixed(2) }} {{ item.currency.symbol }}</span>
-    </template>
-  </CTable>
+  <AccountTable />  
 </template>
